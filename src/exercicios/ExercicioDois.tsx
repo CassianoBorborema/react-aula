1855093f5b746abfcc385a538be1ea8875f50d89
CassianoBorborema <ladeiacassiano@gmail.com>
2026-04-24 10:21:15 -0300

    Exercício 2 Completo

--- src/exercicios/ExercicioDois.tsx
+++ src/exercicios/ExercicioDois.tsx
@@ -1,11 +1,57 @@
-﻿import ExercicioPlaceholder from "../components/ExercicioPlaceholder";
+﻿import { useState } from "react";
 
 function ExercicioDois() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [enviado, setEnviado] = useState(false);
+
+  const formularioValido = nome.length >= 3 && email.includes("@");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setEnviado(true);
+    setNome("");
+    setEmail("");
+  };
+
   return (
-    <ExercicioPlaceholder
-      titulo="Exercicio 02 - Formulario de Cadastro Simples"
-      arquivoMarkdown="02-formulario-de-cadastro-simples.md"
-    />
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h1>
+            <label>
+              text input:{" "}
+              <input
+                name="nome"
+                type="text"
+                value={nome}
+                onChange={(e) => {
+                  setNome(e.target.value);
+                  setEnviado(false);
+                }}
+              />
+            </label>
+            <label>
+              text input:{" "}
+              <input
+                name="email"
+                type="text"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setEnviado(false);
+                }}
+              />
+            </label>
+          </h1>
+          <button type="submit" disabled={!formularioValido}>
+            Enviar
+          </button>
+          {enviado && <p> Sucesso</p>}
+          {!enviado && <p>Falha</p>}
+        </div>
+      </form>
+    </>
   );
 }
 
